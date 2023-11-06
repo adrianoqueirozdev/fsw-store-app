cd2e513866968f482edcd54cc513b43f80afca69
adrianoqueirozdev <adrianoqueirozdev@gmail.com>
2023-11-06 13:18:41 -0300

    feat: first commit

---
+++ lib/main.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:fsw_store/presentation/home/screen/home_screen.dart';
+import 'package:fsw_store/shared/theme/app_colors.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: AppColors.primary),
+        useMaterial3: true,
+        scaffoldBackgroundColor: AppColors.secondary,
+        appBarTheme: AppBarTheme(
+          backgroundColor: AppColors.secondary,
+          foregroundColor: Colors.white,
+        ),
+      ),
+      home: const HomeScreen(),
+    );
+  }
+}
---
+++ lib/shared/theme/app_colors.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+sealed class AppColors {
+  static Color primary = const Color(0xFF5033c3);
+  static Color secondary = const Color(0xFF0B0B0B);
+  static const onSecondaryContainer = Color(0xFF1a1a1a);
+  static const tertiary = Color(0xFF2a2a2a);
+}

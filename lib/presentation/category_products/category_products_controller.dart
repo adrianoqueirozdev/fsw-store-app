b6571089756af58cf601b7db6562ecd9d0424fe9
adrianoqueirozdev <adrianoqueirozdev@gmail.com>
2023-11-14 14:40:00 -0300

    perf: remove unnecessary code

--- lib/presentation/category_products/category_products_controller.dart
+++ lib/presentation/category_products/category_products_controller.dart
@@ -1,4 +1,3 @@
-import 'package:fsw_store/main.dart';
 import 'package:fsw_store/presentation/_blocs/category_products/category_products_bloc.dart';
 import 'package:fsw_store/presentation/_blocs/category_products/category_products_events.dart';
 import 'package:get/get.dart';
@@ -11,8 +10,6 @@ class CategoryProductsController extends GetxController {
     final slug = Get.arguments as String;
 
     categoryProductsBloc = CategoryProductsBloc();
-
-    logger.i(slug);
 
     categoryProductsBloc.add(GetCategoryProductsEvent(slug: slug));
     super.onInit();

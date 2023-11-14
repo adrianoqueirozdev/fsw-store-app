9e529ce3e1d71be677eeefbc72d1b822bf151f41
adrianoqueirozdev <adrianoqueirozdev@gmail.com>
2023-11-14 17:03:26 -0300

    feat: add click navigation on catalog screen

--- lib/presentation/catalog/widgets/catalog_list.dart
+++ lib/presentation/catalog/widgets/catalog_list.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:fsw_store/data/models/category.dart';
+import 'package:fsw_store/shared/constants/routes.dart';
 import 'package:fsw_store/shared/theme/app_colors.dart';
 import 'package:fsw_store/shared/widgets/cached_network_image_widget.dart';
+import 'package:get/get.dart';
 
 class CatalogList extends StatelessWidget {
   final List<Category> categories;
@@ -23,46 +25,49 @@ class CatalogList extends StatelessWidget {
       itemBuilder: (context, index) {
         final category = categories[index];
 
-        return Stack(
-          fit: StackFit.expand,
-          children: [
-            Container(
-              padding: const EdgeInsets.only(left: 32, top: 24, right: 32, bottom: 56),
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(8),
-                gradient: LinearGradient(
-                  colors: [
-                    AppColors.primary,
-                    const Color.fromRGBO(80, 51, 195, 0.3),
-                  ],
-                  begin: Alignment.bottomLeft,
-                  end: Alignment.topRight,
-                ),
-              ),
-              child: CachedNetworkImageWidget(imageUrl: category.imageUrl!),
-            ),
-            Positioned(
-              bottom: 0,
-              child: Container(
-                padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 8),
-                width: size.width / 2 - 22,
-                decoration: const BoxDecoration(
-                  color: AppColors.onSecondaryContainer,
-                  borderRadius: BorderRadius.only(
-                    bottomLeft: Radius.circular(8),
-                    bottomRight: Radius.circular(8),
+        return GestureDetector(
+          onTap: () => Get.toNamed(Routes.categoryProducts, arguments: category.slug),
+          child: Stack(
+            fit: StackFit.expand,
+            children: [
+              Container(
+                padding: const EdgeInsets.only(left: 32, top: 24, right: 32, bottom: 56),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(8),
+                  gradient: LinearGradient(
+                    colors: [
+                      AppColors.primary,
+                      const Color.fromRGBO(80, 51, 195, 0.3),
+                    ],
+                    begin: Alignment.bottomLeft,
+                    end: Alignment.topRight,
                   ),
                 ),
-                child: Text(
-                  category.name!,
-                  textAlign: TextAlign.center,
-                  style: textTheme.titleSmall?.copyWith(
-                    fontWeight: FontWeight.bold,
+                child: CachedNetworkImageWidget(imageUrl: category.imageUrl!),
+              ),
+              Positioned(
+                bottom: 0,
+                child: Container(
+                  padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 8),
+                  width: size.width / 2 - 22,
+                  decoration: const BoxDecoration(
+                    color: AppColors.onSecondaryContainer,
+                    borderRadius: BorderRadius.only(
+                      bottomLeft: Radius.circular(8),
+                      bottomRight: Radius.circular(8),
+                    ),
+                  ),
+                  child: Text(
+                    category.name!,
+                    textAlign: TextAlign.center,
+                    style: textTheme.titleSmall?.copyWith(
+                      fontWeight: FontWeight.bold,
+                    ),
                   ),
                 ),
-              ),
-            )
-          ],
+              )
+            ],
+          ),
         );
       },
     );
